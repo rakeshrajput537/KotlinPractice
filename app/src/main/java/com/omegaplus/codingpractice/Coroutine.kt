a3e92fecdc2d95b278ad1a5374a945f0321ec944
Rakesh Kumar <rakeshrajput537@gmail.com>
2022-06-15 17:25:42 +0530

    suspend function

--- app/src/main/java/com/omegaplus/codingpractice/Coroutine.kt
+++ app/src/main/java/com/omegaplus/codingpractice/Coroutine.kt
@@ -1,23 +1,41 @@
 package com.omegaplus.codingpractice
-import kotlinx.coroutines.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlin.concurrent.thread
 
 fun main(){
 //    mainThread()
 //    workerThread()
-    coroutine()
+//    coroutine()
+    suspendFunction()
+}
+
+fun suspendFunction() {
+    GlobalScope.launch {
+        println("Hi First")
+        printName()
+        println("Hi end")
+
+    }
+    Thread.sleep(3000)
+}
+suspend fun printName(){
+    delay(1000)
+    println("Raka")
 }
 
 fun coroutine() {
 
     //1. create a background coroutine that runs on a background thread
-    println("Thread ${Thread.currentThread().name}: Started")
+    /* println("Thread ${Thread.currentThread().name}: Started")
     GlobalScope.launch {
         println("Thread ${Thread.currentThread().name}: Started")
-        Thread.sleep(1000) // For do some work.... may be File uploading
+        delay(1000) // For do some work.... may be File uploading
         println("Thread ${Thread.currentThread().name}: End")
     }
-    println("Thread ${Thread.currentThread().name}: End")
+    println("Thread ${Thread.currentThread().name}: End") */
 
     // Output : Application does not wait for finish the coroutine
     /* Thread main: Started
@@ -26,14 +44,16 @@ fun coroutine() {
 
 
     //2. create a background coroutine that runs on a background thread
-//    println("Thread ${Thread.currentThread().name}: Started")
-//    GlobalScope.launch {
-//        println("Thread ${Thread.currentThread().name}: Started")
-//        Thread.sleep(1000)
-//        println("Thread ${Thread.currentThread().name}: End")
-//    }
-//    Thread.sleep(2000) // wait for finish the coroutine but not use this in practically
-//    println("Thread ${Thread.currentThread().name}: End")
+    /* println("Thread ${Thread.currentThread().name}: Started")
+
+
+    GlobalScope.launch {
+        println("Thread ${Thread.currentThread().name}: Started")
+        delay(1000)
+        println("Thread ${Thread.currentThread().name}: End")
+    }
+    Thread.sleep(2000) // wait for finish the coroutine but not use this in practically
+    println("Thread ${Thread.currentThread().name}: End") */
 
     // Output
     /* Thread main: Started
@@ -42,7 +62,27 @@ fun coroutine() {
         Thread main: End
 
         Process finished with exit code 0 */
-}
+
+
+
+    // create a thread
+    /* thread {
+        Thread.sleep(1000)
+    } */
+
+    // Operates within a thread
+    /* GlobalScope.launch {
+        Thread.sleep(1000)
+    } */
+
+    // Difference between delay() and Thread.sleep()
+    // delay suspend only coroutine but Thread.sleep blocked the thread
+    // on a thread multiple coroutine can run
+
+    GlobalScope.launch {  }  // non blocking in nature//
+    runBlocking {  } // blocks thread in which it operates
+
+ }
 
 fun workerThread() {
     println("Thread ${Thread.currentThread().name}: Started")
